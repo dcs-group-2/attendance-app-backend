fe117fc8367fadb6dc749fc1c4793f468ed4e22c
Joachim Dekker <jdekker3008@gmail.com>
2025-03-04 21:14:59 +0100

    fix: handle single role claims in authentication checks
    Previously, the authentication service assumed "roles" claims were always lists, causing issues for single role strings. This update adds handling for single role claims to ensure proper validation against required roles.

--- AttendanceSystem.Api/AuthenticationService.cs
+++ AttendanceSystem.Api/AuthenticationService.cs
@@ -8,12 +8,23 @@ public class AuthenticationService(ILogger<AuthenticationService> logger, JsonWe
 {
     public async Task<bool> IsAuthenticated(TokenValidationResult jwt, IEnumerable<string> requiredRoles)
     {
+        if (!jwt.Claims.TryGetValue("roles", out object? rolesClaim)) return false;
+
         // Get the roles from the claims
-        var roles = jwt.Claims["roles"] as IEnumerable<string>;
-        if (roles == null) return false;
-        
+        var roles = rolesClaim as List<object>;
+        if (roles is null)
+        {
+            // It could be that there is just one role
+            var role = rolesClaim as string;
+
+            if (role is not null && requiredRoles.Contains(role)) return true;
+
+            return false;
+        }
+
+
         var isAuthenticated = roles.Any(requiredRoles.Contains);
-        
+
         return jwt.IsValid && isAuthenticated;
     }
 }
