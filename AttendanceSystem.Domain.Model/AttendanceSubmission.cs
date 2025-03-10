030e28ad2809a7a7f47d43abbff1ff7417962bf1
Joachim Dekker <jdekker3008@gmail.com>
2025-03-10 23:22:44 +0100

    feat: refactor AttendanceSubmission to use private field for Attendance for EF and add NotMapped attribute for IsSubmitted.

--- AttendanceSystem.Domain.Model/AttendanceSubmission.cs
+++ AttendanceSystem.Domain.Model/AttendanceSubmission.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,21 @@ namespace AttendanceSystem.Domain.Model
     [method: SetsRequiredMembers]
     public record AttendanceSubmission()
     {
+        private AttendanceKind _attendance = AttendanceKind.Unknown;
+        
         public required AttendanceKind Attendance
         {
-            get;
+            get => _attendance;
             set
             {
-                field = value;
+                _attendance = value;
                 Submitted = DateTime.Now;
             }
-        } = AttendanceKind.Unknown;
+        }
 
         public DateTime? Submitted { get; private set; }
 
+        [NotMapped]
         public bool IsSubmitted => Submitted != null;
     }
 }
